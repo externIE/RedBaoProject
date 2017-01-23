1546fc902a6fde4aeca122f81c306b073906deee
externIE <externIE@outlook.com>
2017-01-23 23:50:09 +0800

    fix一些问题

--- app/src/main/java/com/example/externie/redbaoproject/RedBaoService.java
+++ app/src/main/java/com/example/externie/redbaoproject/RedBaoService.java
@@ -15,9 +15,9 @@ import java.util.Set;
  */
 public class RedBaoService extends AccessibilityService {
 
-    private static final String RedBaoPackage    = "com.tencent.mm:id/a4w";//红包的资源ID
-    private static final String BtnKai           = "com.tencent.mm:id/bi3";//按钮“开”的资源ID
-    private static final String BtnBack          = "com.tencent.mm:id/gv"; //回退键的资源ID
+    private static final String RedBaoPackage    = "com.tencent.mm:id/a48";//红包的资源ID
+    private static final String BtnKai           = "com.tencent.mm:id/be_";//按钮“开”的资源ID
+    private static final String BtnBack          = "com.tencent.mm:id/gr"; //回退键的资源ID
 
     //该对象代表了整个窗口视图的快照
     private AccessibilityNodeInfo mRootNodeInfo = null;
@@ -26,9 +26,16 @@ public class RedBaoService extends AccessibilityService {
     //微信红包收集器
     private Set<AccessibilityNodeInfo> MC_RB_Set = new HashSet<AccessibilityNodeInfo>();
 
+    private Set<AccessibilityNodeInfo> RootRes_Set = new HashSet<AccessibilityNodeInfo>();
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
         mRootNodeInfo = accessibilityEvent.getSource();
+
+        RootRes_Set.add(mRootNodeInfo);
+        int size = RootRes_Set.size();
+
+
         if(mRootNodeInfo == null)
             return;//没有获得窗口视图快照
 
@@ -58,10 +65,11 @@ public class RedBaoService extends AccessibilityService {
     /*
     * 窗口状态发生变化*/
     private void onWindowStateChange(){
-        checkRedBaoAndClickIt(null);
+        //checkRedBaoAndClickIt(null);
         checkKaiAndClickIt();
         checkBtnBackAndClickIt();
     }
+
 
     /*
     * 检查并拆掉没有拆过的红包
@@ -78,20 +86,44 @@ public class RedBaoService extends AccessibilityService {
 
         if(nodeList.size()>0) {
             //至少找到一个红包的NodeInfo
-//            Iterator<AccessibilityNodeInfo> it = nodeList.iterator();
-//            while (it.hasNext()) {
-//                AccessibilityNodeInfo tempNode = it.next();
-//                if (RB_OPENED_Set.add(tempNode)) {//存在一个没有打开过的红包，那么我们现在贱贱的打开它吧，哈哈哈。
-//                    System.out.println("找到一个没有打开过的红包");
-//                    showTip("找到一个没有打开过的红包");
-//                    //点击该节点
-//                    tempNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                    break;
-//                } else {
-//                    System.out.println("找到一个已经拆过的红包");
-//                    showTip("找到一个已经拆过的红包");
-//                }
-//            }
+
+            /*
+            *想法一：找到用一个Set集合来存取打开过的NodeInfo，但我发现一个悲剧的事实，就是每次新事件到来
+            * 所带来的NodeList里面的Node对象和存在Set里面的是一摸一样的，也就是说即便是新的红包出现
+            * 只要当前界面的红包节点数小于set里面的红包节点数，set就会拒绝相同的节点存入啦，所以新的红包就被认为是拆过的
+            * 很悲剧！！！所以想法一 Failed！！！
+            */
+
+            /*Iterator<AccessibilityNodeInfo> it = nodeList.iterator();
+            while (it.hasNext()) {
+                AccessibilityNodeInfo tempNode = it.next();
+                if (RB_OPENED_Set.add(tempNode)) {//存在一个没有打开过的红包，那么我们现在贱贱的打开它吧，哈哈哈。
+                    System.out.println("找到一个没有打开过的红包");
+                    showTip("找到一个没有打开过的红包");
+                    //点击该节点
+                    tempNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break;
+                } else {
+                    System.out.println("找到一个已经拆过的红包");
+                    showTip("找到一个已经拆过的红包");
+                }
+            }*/
+
+            /*
+            * 想法二：不管三七二十一，把当前界面最下面的一个红包节点打开
+            * 这样做的话，在windowState事件里面就不能有判断红包节点的动作，要不然就会无限循环啦
+            * 可能有办法解决这样的无限循环
+            * 于是我把windowState事件里面的红包检查给去掉了
+            * 只剩下contentChange事件里面的红包检查
+            * 这同样带来一个问题就是，你不能滑动你的屏幕啦，滑动就会默认去点击最后一个红包，即便它被拆过
+            * 还有，当你第一次进入群的时候，service是不会去点击没有抢过的红包的
+            * 只有有人新发了红包service才能正常工作
+            * 也就是说在想法二下你需要这么做
+            * 1。保持屏幕常亮
+            * 2。不动手机
+            * 3。默默等红包
+            * 虽然很傻，但至少它开始工作了。。。
+            * */
             AccessibilityNodeInfo lastOne = nodeList.get(nodeList.size() - 1);
             lastOne.performAction(AccessibilityNodeInfo.ACTION_CLICK);
             System.out.println("找到最新的一个，并拆开");
@@ -157,5 +189,6 @@ public class RedBaoService extends AccessibilityService {
 
     private void showTip(String strTip){
         Toast.makeText(this, strTip, Toast.LENGTH_SHORT).show();
+        System.out.print("externIE: "+strTip);
     }
 }
